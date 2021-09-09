bdcebe443c9ab9abd11c099ec76b9d9ea3225b25
alvaro aquije -from *nix <alvarodevcode@outlook.com>
2021-09-09 00:33:16 -0500

    feat(pages): :lipstick: updated UI and added details

--- components/UserDefinedPoke.tsx
+++ components/UserDefinedPoke.tsx
@@ -1,19 +1,39 @@
 import { Box, Grid, Heading } from "@chakra-ui/layout";
 import { FC } from "react";
 import Image from "next/image";
-import { Badge, Button } from "@chakra-ui/react";
+import { Badge, Button, Flex, Tag } from "@chakra-ui/react";
 type Props = {
   poke: any;
 };
 
 export const UserDefinedPoke: FC<Props> = ({ poke }) => {
   return (
-    <Grid placeItems="center" gap={5}>
-      <Heading textTransform="capitalize">{poke.name}</Heading>
-      {poke.sprites.front_default && (
-        <Image src={poke.sprites.front_default} width={100} height={100} />
-      )}
-      <Badge></Badge>
+    <Grid
+      placeItems="center"
+      gap={5}
+      p={10}
+      bgColor="#343d64"
+      rounded="xl"
+      width="md"
+    >
+      <Heading color="white" textTransform="capitalize">
+        {poke.name}
+      </Heading>
+      <Box>
+        {poke.sprites.front_default && (
+          <Image src={poke.sprites.front_default} width={100} height={100} />
+        )}
+        {poke.sprites.back_default && (
+          <Image src={poke.sprites.back_default} height={100} width={100} />
+        )}
+      </Box>
+      <Flex>
+        {poke.abilities.map((a) => (
+          <Tag key={a.slot} colorScheme="teal" mx="3">
+            {a.ability.name}
+          </Tag>
+        ))}
+      </Flex>
     </Grid>
   );
 };
