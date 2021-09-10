a5ecebe9106f2fdc8136cf613c17f1e87aa44064
alvaro aquije -from *nix <alvarodevcode@outlook.com>
2021-09-10 10:23:05 -0500

    fix(utils): :bug: fixed last commit

--- utils/getPokemon.ts
+++ utils/getPokemon.ts
@@ -3,8 +3,7 @@ export const BASE_URL = "https://pokeapi.co/api/v2/";
 export const getSpecificPoke = async (query: string) => {
   console.log(`You are searching for ${query}`);
   try {
-    query.toLowerCase();
-    const fetching = await fetch(`${BASE_URL}pokemon/${query}`);
+    const fetching = await fetch(`${BASE_URL}pokemon/${query.toLowerCase()}`);
     const data = fetching.json();
     return data;
   } catch (err) {
