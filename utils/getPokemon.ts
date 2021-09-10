73a78f91a7d2fed84e973cf44178c3146c15df36
alvaro aquije -from *nix <alvarodevcode@outlook.com>
2021-09-10 10:22:03 -0500

    fix(utils): :ambulance: hotfix of searching pokemons

--- utils/getPokemon.ts
+++ utils/getPokemon.ts
@@ -1,8 +1,9 @@
 export const BASE_URL = "https://pokeapi.co/api/v2/";
 
-export const getSpecificPoke = async (query) => {
+export const getSpecificPoke = async (query: string) => {
   console.log(`You are searching for ${query}`);
   try {
+    query.toLowerCase();
     const fetching = await fetch(`${BASE_URL}pokemon/${query}`);
     const data = fetching.json();
     return data;
